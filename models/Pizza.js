d5e15a34cb6b6bdf205880f3a86f2379c23df28e
Vox Humana <jonprine@gmail.com>
2020-10-05 10:04:27 -0500

    update

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,9 +5,13 @@ const moment = require('moment');
 const PizzaSchema = new Schema({
   pizzaName: {
     type: String,
+    required: true,
+    trim: true
   },
   createdBy: {
     type: String,
+    required: true,
+    trim: true
   },
   createdAt: {
     type: Date,
@@ -16,6 +20,7 @@ const PizzaSchema = new Schema({
   },
   size: {
     type: String,
+    enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
     default: "Large",
   },
   toppings: [],
@@ -36,9 +41,9 @@ const PizzaSchema = new Schema({
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual("commentCount").get(function () {
-  return this.comments.length;
-});
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+  });
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model("Pizza", PizzaSchema);
